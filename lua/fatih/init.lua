980c7ac872a9c543046bbcce07122f8de8d667c7
Fatih Aslancan <fthaslncn@gmail.com>
2023-02-15 19:13:52 +0300

    update

--- lua/fatih/init.lua
+++ lua/fatih/init.lua
@@ -39,44 +39,45 @@ require("lazy").setup({ 'folke/tokyonight.nvim',
   },{
     'glepnir/dashboard-nvim',
     event = 'VimEnter',
-    config = function()
-      require('dashboard').setup({
-        theme = 'hyper',
-        config = {
-          week_header = {
-           enable = true,
-          },
-          shortcut = {
-            { desc = ' Update', group = '@property', action = 'Lazy update', key = 'u' },
-            {
-              icon = ' ',
-              icon_hl = '@variable',
-              desc = 'Files',
-              group = 'Label',
-              action = 'Telescope find_files',
-              key = 'f',
-            },
-            {
-              desc = ' Apps',
-              group = 'DiagnosticHint',
-              action = 'Telescope app',
-              key = 'a',
-            },
-            {
-              desc = ' dotfiles',
-              group = 'Number',
-              action = 'Telescope dotfiles',
-              key = 'd',
-            },
-          },
-        },
-      })
-    end,
     dependencies = { {'nvim-tree/nvim-web-devicons'}}
   },{'romgrk/barbar.nvim', dependencies = 'nvim-tree/nvim-web-devicons'},{'nvim-tree/nvim-tree.lua', dependencies = 'nvim-tree/nvim-web-devicons'},  {'nvim-lualine/lualine.nvim', dependencies = 'nvim-tree/nvim-web-devicons'},'tpope/vim-fugitive', 'mbbill/undotree', 'folke/zen-mode.nvim'})
 
 --
 require("tokyonight").setup()
+
+--
+
+require('dashboard').setup({
+  theme = 'hyper',
+  config = {
+    week_header = {
+     enable = true,
+    },
+    shortcut = {
+      { desc = ' Update', group = '@property', action = 'Lazy update', key = 'u' },
+      {
+        icon = ' ',
+        icon_hl = '@variable',
+        desc = 'Files',
+        group = 'Label',
+        action = 'Telescope find_files',
+        key = 'f',
+      },
+      {
+        desc = ' Apps',
+        group = 'DiagnosticHint',
+        action = 'Telescope app',
+        key = 'a',
+      },
+      {
+        desc = ' dotfiles',
+        group = 'Number',
+        action = 'Telescope dotfiles',
+        key = 'd',
+      },
+    },
+  },
+})
 
 
 vim.cmd [[colorscheme tokyonight-night]]
