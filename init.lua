340909a051253577f15825fe7414d1727fb56827
alazul <fthaslncn@gmail.com>
2023-02-17 02:27:12 +0300

    new update

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 vim.api.nvim_exec('language en_US', true)
 
-vim.opt.guifont = { "FiraCode NF", ":h12"}
+vim.opt.guifont = { "FiraCode NF", ":h11"}
 
 vim.g.neovide_refresh_rate = 60
 
--- lua/fatih/init.lua
+++ lua/fatih/init.lua
@@ -10,11 +10,21 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
-
 vim.g.mapleader = " "
 
 require("lazy").setup({ 'folke/tokyonight.nvim',
-  { 'nvim-telescope/telescope.nvim', dependencies = { { 'nvim-lua/plenary.nvim' } } }, 'nvim-treesitter/nvim-treesitter',
+  { 'nvim-telescope/telescope.nvim', dependencies = { { 'nvim-lua/plenary.nvim' } } }, {'nvim-treesitter/nvim-treesitter'},{
+  "utilyre/barbecue.nvim",
+  name = "barbecue",
+  version = "*",
+  dependencies = {
+    "SmiteshP/nvim-navic",
+    "nvim-tree/nvim-web-devicons", -- optional dependency
+  },
+  opts = {
+    -- configurations go here
+  },
+},'gelguy/wilder.nvim','rcarriga/nvim-notify',
   {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v1.x',
@@ -36,17 +46,65 @@ require("lazy").setup({ 'folke/tokyonight.nvim',
       { 'L3MON4D3/LuaSnip' }, -- Required
       { 'rafamadriz/friendly-snippets' }, -- Optional
     }
-  },{
+  },
+  {'akinsho/toggleterm.nvim'},{'folke/neodev.nvim'},{'folke/trouble.nvim'},
+  {'onsails/lspkind.nvim'},{'folke/which-key.nvim',config = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+      require("which-key").setup({
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      })
+    end},{'lewis6991/gitsigns.nvim'},
+  {
     'glepnir/dashboard-nvim',
     event = 'VimEnter',
     dependencies = { {'nvim-tree/nvim-web-devicons'}}
-  },{'romgrk/barbar.nvim', dependencies = 'nvim-tree/nvim-web-devicons'},{'nvim-tree/nvim-tree.lua', dependencies = 'nvim-tree/nvim-web-devicons'},  {'nvim-lualine/lualine.nvim', dependencies = 'nvim-tree/nvim-web-devicons'},'tpope/vim-fugitive', 'mbbill/undotree', 'folke/zen-mode.nvim'})
+  },{'akinsho/bufferline.nvim', dependencies = 'nvim-tree/nvim-web-devicons'},{'nvim-tree/nvim-tree.lua', dependencies = 'nvim-tree/nvim-web-devicons'},  {'nvim-lualine/lualine.nvim', dependencies = 'nvim-tree/nvim-web-devicons'},'tpope/vim-fugitive', 'mbbill/undotree', 'folke/zen-mode.nvim'})
 
 --
 require("tokyonight").setup()
-
 --
 
+
+-- set termguicolors for opacity animation
+vim.opt.termguicolors = true
+
+require("notify").setup({
+  -- Animation style (see below for details)
+  stages = "fade",
+
+  -- Function called when a new window is opened, use for changing win settings/config
+  -- on_open = nil,
+
+  -- Function called when a window is closed
+  --on_close = nil,
+
+  -- Render function for notifications. See notify-render()
+  render = "compact",
+
+  -- Default timeout for notifications
+  -- timeout = 4999,
+
+  -- For stages that change opacity this is treated as the highlight behind the window
+  -- Set this to either a highlight group, an RGB hex value e.g. "#-000001" or a function returning an RGB code for dynamic values
+  -- background_colour = "Normal",
+
+  -- Minimum width for notification windows
+  -- minimum_width = 49,
+
+  -- Icons for the different levels
+  --icons = {
+   -- ERROR = "",
+   -- WARN = "",
+   -- INFO = "",
+   -- DEBUG = "",
+   -- TRACE = "✎",
+ -- },
+})
+vim.notify = require("notify")
+--
 require('dashboard').setup({
   theme = 'hyper',
   config = {
@@ -79,17 +137,16 @@ require('dashboard').setup({
   },
 })
 
-
 vim.cmd [[colorscheme tokyonight-night]]
 --
 --
 local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<C-space>', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 --
-
 -- treesitter
 require 'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the four listed parsers should always be installed)
@@ -134,6 +191,11 @@ require 'nvim-treesitter.configs'.setup {
   },
 }
 --
+-- IMPORTANT: make sure to setup neodev BEFORE lspconfig
+require("neodev").setup({
+  -- add any options here, or leave empty to use the default settings
+})
+
 --
 local lsp = require("lsp-zero")
 
@@ -148,8 +210,61 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<M-k>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<M-j>'] = cmp.mapping.select_next_item(cmp_select),
   ['<M-i>'] = cmp.mapping.confirm({ select = true }),
+  --['<C-space>'] = cmp.mapping.confirm()
 })
-lsp.setup_nvim_cmp({mapping=cmp_mappings})
+
+
+lsp.setup_nvim_cmp({mapping=cmp_mappings,formatting = {
+    format = function(entry, vim_item)
+      if vim.tbl_contains({ 'path' }, entry.source.name) then
+        local icon, hl_group = require('nvim-web-devicons').get_icon(entry:get_completion_item().label)
+        if icon then
+          vim_item.kind = icon
+          vim_item.kind_hl_group = hl_group
+          return vim_item
+        end
+      end
+      local cmp_kinds = {
+  Text = '  ',
+  Method = '  ',
+  Function = '  ',
+  Constructor = '  ',
+  Field = '  ',
+  Variable = '  ',
+  Class = '  ',
+  Interface = '  ',
+  Module = '  ',
+  Property = '  ',
+  Unit = '  ',
+  Value = '  ',
+  Enum = '  ',
+  Keyword = '  ',
+  Snippet = '  ',
+  Color = '  ',
+  File = '  ',
+  Reference = '  ',
+  Folder = '  ',
+  EnumMember = '  ',
+  Constant = '  ',
+  Struct = '  ',
+  Event = '  ',
+  Operator = '  ',
+  TypeParameter = '  ',
+}
+      vim.cmd('highlight! CmpItemAbbrDeprecated guibg=NONE gui=strikethrough guifg=#808080')
+vim.cmd('highlight! CmpItemAbbrMatch guibg=NONE guifg=#569CD6')
+vim.cmd('highlight! CmpItemAbbrMatchFuzzy guibg=NONE guifg=#569CD6')
+vim.cmd('highlight! CmpItemKindVariable guibg=NONE guifg=#9CDCFE')
+vim.cmd('highlight! CmpItemKindInterface guibg=NONE guifg=#9CDCFE')
+vim.cmd('highlight! CmpItemKindText guibg=NONE guifg=#9CDCFE')
+vim.cmd('highlight! CmpItemKindFunction guibg=NONE guifg=#C586C0')
+vim.cmd('highlight! CmpItemKindMethod guibg=NONE guifg=#C586C0')
+vim.cmd('highlight! CmpItemKindKeyword guibg=NONE guifg=#D4D4D4')
+ vim_item.kind = (cmp_kinds[vim_item.kind] or '') .. vim_item.kind
+      return vim_item
+      --return require('lspkind').cmp_format({ with_text = true })(entry, vim_item)
+    end
+  }})
 
 lsp.set_preferences({
   suggest_lsp_servers = false,
@@ -160,7 +275,6 @@ lsp.set_preferences({
         info = 'I'
     }
 })
-
 lsp.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
@@ -193,9 +307,8 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
 -- empty setup using defaults
-require("nvim-tree").setup()
-
-
+local nvimTree = require("nvim-tree").setup()
+vim.keymap.set("n","<C-b>","<cmd>NvimTreeToggle<cr>",{})
 --
 
 require('lualine').setup{
@@ -206,5 +319,158 @@ require('lualine').setup{
 
 --
 
+vim.opt.termguicolors = true
+require('bufferline').setup({options = {
+  
+}})
 
-require('bufferline').setup()
+
+--
+require("toggleterm").setup()
+vim.keymap.set("n","<C-t>","<cmd>ToggleTerm<cr>",{})
+--
+
+--
+require('gitsigns').setup {
+  signs = {
+    add          = { text = '│' },
+    change       = { text = '│' },
+    delete       = { text = '_' },
+    topdelete    = { text = '‾' },
+    changedelete = { text = '~' },
+    untracked    = { text = '┆' },
+  },
+  signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
+  numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
+  linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
+  word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
+  watch_gitdir = {
+    interval = 1000,
+    follow_files = true
+  },
+  attach_to_untracked = true,
+  current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  current_line_blame_opts = {
+    virt_text = true,
+    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+    delay = 1000,
+    ignore_whitespace = false,
+  },
+  current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+  sign_priority = 6,
+  update_debounce = 100,
+  status_formatter = nil, -- Use default
+  max_file_length = 40000, -- Disable if file is longer than this (in lines)
+  preview_config = {
+    -- Options passed to nvim_open_win
+    border = 'single',
+    style = 'minimal',
+    relative = 'cursor',
+    row = 0,
+    col = 1
+  },
+  yadm = {
+    enable = false
+  },
+}
+--
+
+--
+local trouble = require("trouble").setup({
+    position = "bottom", -- position of the list can be: bottom, top, left, right
+    height = 10, -- height of the trouble list when position is top or bottom
+    width = 50, -- width of the list when position is left or right
+    icons = true, -- use devicons for filenames
+    mode = "workspace_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
+    fold_open = "", -- icon used for open folds
+    fold_closed = "", -- icon used for closed folds
+    group = true, -- group results by file
+    padding = true, -- add an extra new line on top of the list
+    action_keys = { -- key mappings for actions in the trouble list
+        -- map to {} to remove a mapping, for example:
+        -- close = {},
+        close = "q", -- close the list
+        cancel = "<esc>", -- cancel the preview and get back to your last window / buffer / cursor
+        refresh = "r", -- manually refresh
+        jump = {"<cr>", "<tab>"}, -- jump to the diagnostic or open / close folds
+        open_split = { "<c-x>" }, -- open buffer in new split
+        open_vsplit = { "<c-v>" }, -- open buffer in new vsplit
+        open_tab = { "<c-t>" }, -- open buffer in new tab
+        jump_close = {"o"}, -- jump to the diagnostic and close the list
+        toggle_mode = "m", -- toggle between "workspace" and "document" diagnostics mode
+        toggle_preview = "P", -- toggle auto_preview
+        hover = "K", -- opens a small popup with the full multiline message
+        preview = "p", -- preview the diagnostic location
+        close_folds = {"zM", "zm"}, -- close all folds
+        open_folds = {"zR", "zr"}, -- open all folds
+        toggle_fold = {"zA", "za"}, -- toggle fold of current file
+        previous = "k", -- previous item
+        next = "j" -- next item
+    },
+    indent_lines = true, -- add an indent guide below the fold icons
+    auto_open = false, -- automatically open the list when you have diagnostics
+    auto_close = false, -- automatically close the list when you have no diagnostics
+    auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
+    auto_fold = false, -- automatically fold a file trouble list at creation
+    auto_jump = {"lsp_definitions"}, -- for the given modes, automatically jump if there is only a single result
+    signs = {
+        -- icons / text used for a diagnostic
+        error = "",
+        warning = "",
+        hint = "",
+        information = "",
+        other = "﫠"
+    },
+    use_diagnostic_signs = false -- enabling this will use the signs defined in your lsp client
+})
+vim.keymap.set("n", '<C-E>', "<cmd>TroubleToggle<cr>",
+  {silent = true, noremap = true}
+)
+--
+
+-- triggers CursorHold event faster
+vim.opt.updatetime = 200
+
+require("barbecue").setup({
+  create_autocmd = false, -- prevent barbecue from updating itself automatically
+})
+
+vim.api.nvim_create_autocmd({
+  "WinScrolled", -- or WinResized on NVIM-v0.9 and higher
+  "BufWinEnter",
+  "CursorHold",
+  "InsertLeave",
+
+  -- include these if you have set `show_modified` to `true`
+  "BufWritePost",
+  "TextChanged",
+  "TextChangedI",
+}, {
+  group = vim.api.nvim_create_augroup("barbecue.updater", {}),
+  callback = function()
+    require("barbecue.ui").update()
+  end,
+})
+--
+local wilder = require('wilder')
+wilder.setup({modes = {':', '/', '?'}})
+
+wilder.set_option('renderer', wilder.popupmenu_renderer(
+  wilder.popupmenu_border_theme({
+     highlights = {
+      border = 'Normal', -- highlight to use for the border
+    },
+    -- 'single', 'double', 'rounded' or 'solid'
+    -- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
+    border = 'rounded',
+   highlighter = wilder.basic_highlighter(),
+   highlights = {
+    accent = wilder.make_hl('WilderAccent', 'Pmenu', {{a = 1}, {a = 1}, {foreground = '#f4468f'}}),
+  },
+  left = {' ', wilder.popupmenu_devicons()},
+  right = {' ', wilder.popupmenu_scrollbar()}})          
+)
+)
+
+--
+
